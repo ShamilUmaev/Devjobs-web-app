57665e2d40881b6bd973cd079ae5c44128bd838b
Shamil Umaev <shamil.umaev@hotmail.com>
2023-11-09 23:12:31 +0100

    Add a popup with a background overlay to filter the jobs by location and full time

--- js/script.js
+++ js/script.js
@@ -1,20 +1,40 @@
 const checkBox = document.querySelector("input[type='checkbox']");
 const pageOuterContainer = document.querySelector('.page-outer-container');
 const form = document.querySelector('.form');
+const popupBackground = document.querySelector('.popup-background');
 const filterIcon = document.querySelector('.filter-svg');
-const jobCards = document.querySelectorAll('.job-card');
+const card = document.querySelectorAll('.card');
 const jobTitles = document.querySelectorAll('.job-title');
+const filterFormPopupMobile = document.querySelector('.filter-form-popup-mobile');
+const fulltimeLabel = document.querySelector('.fulltime-label');
 
 const darkMode = () => {
     pageOuterContainer.classList.toggle('dark-mode-bg');
     form.classList.toggle('dark-mode-form');
     filterIcon.classList.toggle('filter-icon-dark-mode');
-    jobCards.forEach(card => {
+    card.forEach(card => {
         card.classList.toggle('dark-mode-job-card')
     })
     jobTitles.forEach(title => {
         title.classList.toggle('dark-mode-font');
     })
+    fulltimeLabel.classList.toggle('dark-mode-font');
 }
 
+const showPopup = () => {
+    popupBackground.classList.add('overlay');
+    filterFormPopupMobile.classList.remove('hidden');
+    filterFormPopupMobile.classList.add('visible');
+}
+
+const closePopup = (e) => {
+    if(e.target.classList.contains('overlay')) {
+        popupBackground.classList.remove('overlay');
+        filterFormPopupMobile.classList.remove('visible');
+        filterFormPopupMobile.classList.add('hidden');
+    }
+}
+
+popupBackground.addEventListener('click', closePopup);
+filterIcon.addEventListener('click', showPopup);
 checkBox.addEventListener('change', darkMode);
